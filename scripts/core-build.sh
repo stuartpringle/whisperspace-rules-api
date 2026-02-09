35d1c80ef34c9d8b1ea7efd3dd850bdf85eccf0a
Stuart Pringle <kaboodleschmitt@hotmail.com>
2026-02-09 16:45:45 -0500

    fix(rules-api): use local tsc

--- scripts/core-build.sh
+++ scripts/core-build.sh
@@ -8,7 +8,7 @@ rm -rf "$REPO_OUT"
 mkdir -p "$REPO_OUT"
 
 echo "[core:build] Building core module to ${REPO_OUT}"
-npx tsc -p packages/core/tsconfig.json --outDir "$REPO_OUT"
+./node_modules/.bin/tsc -p packages/core/tsconfig.json --outDir "$REPO_OUT"
 
 mkdir -p "$PUBLISH_OUT"
 rm -rf "$PUBLISH_OUT"
